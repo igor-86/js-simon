4368edece1493d882af91bf129772603e214764b
igor-86 <i.campoli86@gmail.com>
2022-10-19 17:15:56 +0200

    Pseudo codice

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ESERCIZIO : pseudo codice svolgimento */
+//Creo un array di numeri random
+ // Creo una funzione che mi fa togliere dallo schermo i numeri dopo 30 secondi.
+// Dopo che i numeri sono spariti: Chiedo mediante un ciclo while 5 numeri all'utente
+    //Riempio un array vuoto con i 5 numeri indicati dall'utente
+
+//Confronto i 5 numeri nell'array random con i 5 numeri dell'array vuoto, riempito con numeri dall'utente
+    //per farlo Trasformo gli elementi numerici nell'array in stringhe per poterli confrontare piu facilmente*****
+
+    // SE le stringhe contenute nei due array sono uguali
+        // Stampo la comunicazione "Bravo hai vinto"
+    // ALTRIMENTI
+        // Stampo la comunicazione "hai una brutta memoria, i numeri erano + ecc ecc..."
